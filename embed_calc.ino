ecb336046849aa44e82d7942b65262357369b318
jTieu <115609391+joTieu@users.noreply.github.com>
2024-07-29 20:09:36 -0400

    Preparing Bitshift for segment displays

--- embed_calc.ino
+++ embed_calc.ino
@@ -3,18 +3,26 @@
 */
 #include <math.h>
 // Variables to declare
-const int decNumber = 18;
+const int decNumber = 12;
 const int numOfBits = floor(log(decNumber)/log(2)) + 1; // Grab the maximum number of bits required to represent the decimal number
 int bitArray[numOfBits];
+int dataPin = A0;
+int latchPin = A1;
+int clockPin = D12;
 
-void clk() {
-  // Read input to D11 (to check if output pin is ON or OFF)
-  if(digitalRead(11) == HIGH) {
-    digitalWrite(12, LOW);
-  } else {
-    digitalWrite(12, HIGH);
-  }
-}
+int digitSegments[10] = {
+  b1111110, // 0
+  b0110000, // 1
+  b1101101, // 2
+  b1111001, // 3
+  b0110011,// 4
+  b1011011,// 5
+  b1011111,// 6
+  b1110000,// 7
+  b1111111,// 8
+  b1111011 // 9
+};
+
 // Converts decimal to binary as an array
 void decToBinary(int n)
 {
@@ -36,42 +44,48 @@ void multiPin(int start, int end, int output) {
 
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, OUTPUT); // Serial Data Output
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(12, OUTPUT); // Clock output
-  pinMode(11, INPUT); // Clock check
+  pinMode(dataPin, OUTPUT); // Serial Data Output
+  pinMode(latchPin, OUTPUT); // Latch pin for shift register
+  // pinMode(A2, OUTPUT);
+  // pinMode(A3, OUTPUT);
+
+  pinMode(clockPin, OUTPUT); // Clock output
+  // pinMode(11, INPUT); // Clock check
+  digitalWrite(clockPin, LOW); // Initialize clock as LOW at start
+  digitalWrite(latchPin, HIGH); // Enables data to be received
   decToBinary(decNumber);
-  digitalWrite(12, LOW); // Initialize clock as LOW at start
 }
 
-void loop() { /*
-  for (int i = 0; i < sizeof(bitArray)/sizeof(int); i++) {
+void loop() { 
+  for (int i = numOfBits - 1; i >= 0; i--) {
     // bitArray[0] will hold the most significant bit (left-most bit) of a binary number, and thus will be first 
     // to output to the LEDs via FIFO queue
 
     // Writes each binary bit into shift register on falling edge
     // (before loading onto next register) as a serial input. 
     // This is to ensure next register is loaded properly on CLK rising edge rather than pushing next bit on falling edge
-    if (digitalRead(D11) == LOW) {
-      digitalWrite(A0, bitArray[i]);  
-    }
-    delay(500);
-    // Shift the bit with the clock cycle before inserting new bit value
-    clk();
-  }*/
-  for(int j = 0; j < 3; j++){
-    for(int k = 0; k < 4; k++) {
-      digitalWrite(14 + k, HIGH);
-      delay(200);
-      digitalWrite(14+k, LOW);
-    }
+    digitalWrite(latchPin, HIGH);
+    shiftOut(dataPin, clockPin, LSBFIRST, decNumber);
+    digitalWrite(latchPin, LOW);
+    delay(2000);
+    // digitalWrite(12, LOW);
+    // digitalWrite(A0, bitArray[i]);  // Write bit to serial input when clock = 0
+    // delay(1000); // Wait 1s
+    // digitalWrite(12, HIGH);
+    // delay(1000);
   }
-  for(int p = 0; p < 4; p++) {
-    multiPin(14,17,1);
-    delay(200);
-    multiPin(14,17,0);
-    delay(200);
-  }
+  digitalWrite(latchPin, LOW);
+  // for(int j = 0; j < 3; j++){
+  //   for(int k = 0; k < 4; k++) {
+  //     digitalWrite(14 + k, HIGH);
+  //     delay(200);
+  //     digitalWrite(14+k, LOW);
+  //   }
+  // }
+  // for(int p = 0; p < 4; p++) {
+  //   multiPin(14,17,1);
+  //   delay(200);
+  //   multiPin(14,17,0);
+  //   delay(200);
+  // }
 }
